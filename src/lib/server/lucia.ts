1f0314b095501626e9b0fb850304cc1b25a1158b
NM711 <nm7994918@gmail.com>
2023-06-29 21:19:58 -0700

    authenthication added

---
+++ src/lib/server/lucia.ts
@@ -0,0 +1,23 @@
+import lucia from "lucia-auth";
+import { sveltekit } from "lucia-auth/middleware";
+import prisma from "@lucia-auth/adapter-prisma";
+import { PrismaClient } from "@prisma/client";
+import { dev } from "$app/environment";
+
+const client = new PrismaClient()
+
+export const auth = lucia({
+	adapter: prisma(client),
+	env: dev ? "DEV" : "PROD",
+	middleware: sveltekit(),
+  transformDatabaseUser: (userData) => {
+    return {
+        id: userData.id,
+        username: userData.username,
+        email: userData.email,
+        picture: userData.picture
+    }
+  }
+});
+
+export type Auth = typeof auth;
