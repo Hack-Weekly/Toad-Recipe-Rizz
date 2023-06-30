b1ba7fbe3d1aa7fe26f6b7f8156ebcaebf0a1f31
Heesel <hesselpa@live.nl>
2023-06-30 20:56:35 +0200

    Auth UI & Validation

--- src/routes/auth/sign-in/+page.server.ts
+++ src/routes/auth/sign-in/+page.server.ts
@@ -7,9 +7,12 @@ export const actions: Actions = {
         const loginCredentials = await request.formData()
         const { username, password } = Object.fromEntries(loginCredentials) as Record<string, string>
         try {
-          const key = auth.useKey("username", username, password)
-          const session = await auth.createSession((await key).userId)
-          locals.auth.setSession(session)
+            if(username.length === 0) return fail(400, { message: "Username is required" })
+            if (password.length === 0) return fail(400, { message: "Password is required" })
+            
+            const key = auth.useKey("username", username, password)
+            const session = await auth.createSession((await key).userId)
+            locals.auth.setSession(session)
         } catch (err) {
             return fail(400, { message: "Invalid Credentials Or User Does Not Exists Try Again!" })
         }
--- src/routes/auth/sign-up/+page.server.ts
+++ src/routes/auth/sign-up/+page.server.ts
@@ -6,12 +6,26 @@ export const actions: Actions = {
     default: async ({ request }) => {
         const newCredentials = await request.formData()
         // key values to string conversion
-        const { email, username, password } = Object.fromEntries(newCredentials) as Record<string, string>
+        const { email, username, password, confirm_password } = Object.fromEntries(newCredentials) as Record<string, string>
         const picture = "Not Added"
- 
-        if (password.length === 0 || username.length === 0 || email.length === 0) {
-              return fail(400, { message: "Form Has Not Been Filled!" })
-        }
+
+        const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        const usernameRegex = /^[a-zA-Z0-9_]+$/
+        const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{6}$/
+
+
+        if(email.length === 0) return fail(400, { message: "Email is required" })
+        if(!emailRegex.test(email)) return fail(400, { message: "Email is format invalid" })
+
+        if(username.length === 0) return fail(400, { message: "Username is required" })
+        if(username.length <= 2) return fail(400, { message: "Username must be more than 2 characters" })
+        if(!usernameRegex.test(username)) return fail(400, { message: "Username can only contain lowercase, uppercase, numbers and underscores" })
+
+        if (password.length === 0) return fail(400, { message: "Password is required" })
+        if (password.length <= 6) return fail(400, { message: "Password must be more than 6 characters" })
+        if (!passwordRegex.test(password)) return fail(400, { message: "Password must contain at least one uppercase letter, one lowercase letter, one number and a special character" })
+        if (password !== confirm_password) return fail(400, { message: "Passwords do not match" })
+        
 
         try {
           const user =  await auth.createUser({
@@ -26,7 +40,6 @@ export const actions: Actions = {
               }
             })
         }  catch (err) {
-            console.log(err, "HI WORLD")
             return fail(400, { message: "This User Already Exists!" })
         }
     }
